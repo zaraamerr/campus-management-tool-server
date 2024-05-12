94c867653d6bdc355a4ea1c377f43451b535b36a
fatimasif <fatimasif2004@gmail.com>
2024-05-12 19:55:32 -0400

    added minor change to allow one campus per student

--- database/models/index.js
+++ database/models/index.js
@@ -6,7 +6,8 @@ It registers models, sets up associations between tables, and generates barrel f
 const Student  = require('./Student');  // Import Student model
 const Campus  = require('./Campus');  // Import Campus model
 
-Student.belongsTo(Campus);  // Student belongs to only one Campus 
+// Student.belongsTo(Campus);  // Student belongs to only one Campus 
+Student.belongsTo(Campus, { foreignKey: { allowNull: true } }); 
 Campus.hasMany(Student);  // Campus can have many Student
 
 // Export models and associations
